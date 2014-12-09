47c5ed17eccc115dbfa6963e1826e1f7aef4a554
ggarcia <ggarcia@eureka.inf.br>
2014-12-08 23:18:26 -0200

    Tokens namespace
    Splitting token related functions and functions that handle request
    into two different namespaces.
    
    Adding tests for functions relate to tokens life-cycle.

--- src/toro_tokens_rest/handler.clj
+++ src/toro_tokens_rest/handler.clj
@@ -4,27 +4,18 @@
   (:use ring.util.response)
   (:require [compojure.handler :as handler]
             [compojure.route :as route]
-            [ring.middleware.json :as middleware]))
-
-(def test-token {:token "44c2617815014a18976866941d42d0ff"
-                 :expiration-date "2014-09-10 23:59:59 GMT-3"
-                 :remaining-usages 1})
-
-(defn valid-token? [token]
-  (= "44c2617815014a18976866941d42d0ff" token))
+            [ring.middleware.json :as middleware]
+            [toro-tokens-rest.tokens :as tkns]))
 
 (defn get-token [token {body :body}]
   (println (body :api-key))
-  (if (valid-token? token)
+  (if (tkns/valid-token? token)
     {:status 200
      :headers {"Content-Type" "application/json; charset=utf-8"}
-     :body test-token}
+     :body (tkns/create-token 1)}
     {:status 404
      :headers {"Content-Type" "application/json; charset=utf-8"}
      :body {:error "Invalid Token"}}))
-
-(defn create-token []
-  (clojure.string/replace (str (java.util.UUID/randomUUID)) "-" ""))
 
 (defn increment-usage-time [token]
   (str "incremented token " token))
@@ -32,7 +23,7 @@
 (defroutes app-routes
            (context "/api/:version/tokens" [api-version]
                     (defroutes tokens-routes
-                               (POST "/" [] (create-token))
+                               (POST "/" [] (tkns/create-token 1))
                                (context "/:token" [token] (defroutes token-routes
                                                                      (GET "/" {body :body} (get-token token body))
                                                                      (POST "/" [] (increment-usage-time token))))))
