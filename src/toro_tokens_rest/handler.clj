027484ec06b5a16df356799b4f52d84f4fdac6f8
PainterAndHacker <nathantjcc@gmail.com>
2015-01-27 14:46:48 -0500

    Removed API key, and created response-json to abstract responses and cut down on code reuse.

--- src/toro_tokens_rest/handler.clj
+++ src/toro_tokens_rest/handler.clj
@@ -7,50 +7,28 @@
             [ring.middleware.json :as middleware]
             [toro-tokens-rest.tokens :as tkns]))
 
-;;temporary API key
-(def api-key "123")
 
-(defn valid-api-key? [key]
-  (= key api-key))
+(defn response-json [status-number body]
+  {:status status-number
+   :headers {"Content-Type" "application/json; charset=utf-8"}
+   :body body})
 
 (defn get-token [token-id]
   (if-let [result (tkns/get-token token-id)]
-    {:status 200
-     :headers {"Content-Type" "application/json; charset=utf-8"}
-     :body (str result)}
-    {:status 404
-     :headers {"Content-Type" "application/json; charset=utf-8"}
-     :body {:error "Invalid token ID"}}))
+    (response-json 200 (str result))
+    (response-json 404 {:error "Invalid token ID"})))
     
-
 (defn create-token 
   "Handler for creating a token. Parses the body, passes it off to tkns/create-token, and then forms the response."
   [body]
-  (if (valid-api-key? (get body "api-key"))   
-    {:status 200
-     :headers {"Content-Type" "application/json; charset=utf-8"}
-     :body (str(tkns/create-token (get body "remaining-usages" 1) (get body "hours-till-expiration" 24) ))}
-    {:status 401
-     :headers {"Content-Type" "application/json; charset=utf-8"}
-     :body {:error "Invalid API key"}}))
+  (response-json 200 (str(tkns/create-token (get body "remaining-usages" 1) (get body "hours-till-expiration" 24) ))))
         
-(defn use-token [body token-id]
-  (if (valid-api-key? (get body "api-key"))
-    (if-let [result (tkns/get-token token-id)]
+(defn use-token [token-id]
+  (if-let [result (tkns/get-token token-id)]
       (if (tkns/valid? result)
-         {:status 200
-          :headers {"Content-Type" "application/json; charset=utf-8"}
-          :body (str (tkns/use! result))}
-         {:status 405
-          :headers {"Content-Type" "application/json; charset=utf-8"}
-          :body (str result)})
-      
-      {:status 404
-       :headers {"Content-Type" "application/json; charset=utf-8"}
-       :body {:error "Invalid token ID"}}) 
-    {:status 401
-       :headers {"Content-Type" "application/json; charset=utf-8"}
-       :body {:error "Invalid API key"}}))
+         (response-json 200 (str (tkns/use! result)))
+         (response-json 405 (str result)))
+      (response-json 404 {:error "Invalid token ID"})))
     
 
     
@@ -60,7 +38,7 @@
                                (POST "/" {b :body} (create-token b))
                                (context "/:token-id" [token-id] (defroutes token-routes
                                                                      (GET "/" [] (get-token token-id))
-                                                                     (POST "/" {b :body} (use-token b token-id))))))
+                                                                     (POST "/" [] (use-token token-id))))))
            (route/not-found "Not Found"))
 
 
